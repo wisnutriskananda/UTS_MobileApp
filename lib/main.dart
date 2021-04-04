8bb37f79a7c899744565714e6112e42c5a35f9ad
wisnutriskananda <triskanandasidartha@gmail.com>
2021-04-04 12:15:15 +0800

    menambahkan class my app

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
 class _MyAppState extends State<MyApp> {
   var questionIndex = 0;
 
