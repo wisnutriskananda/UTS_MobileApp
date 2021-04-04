5fb2fda2c95c7accbdf17d7387f1f56e6516c1c7
wisnutriskananda <triskanandasidartha@gmail.com>
2021-04-04 15:07:57 +0800

    membuat pertanyaan dan membuat answer

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,79 @@
-mport 'package:flutter/material.dart';
- 
+import 'package:flutter/material.dart';
+
 import './quiz.dart';
 import './result.dart';
 // void main() {
 //   runApp(MyApp());
 // }
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatefulWidget {
- @override
- State<StatefulWidget> createState() {
-   // TODO: implement createState
-   return _MyAppState();
- }
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
- 
+
 class _MyAppState extends State<MyApp> {
- final _questions = const [
+  final _questions = const [
     {
-      'questionText': 'Apa warna favorit anda?',
-      'answers': ['Hitam', 'Merah', 'Hijau', 'Putih'],
+      'questionText': 'Apa brand kesukaan anda?',
+      'answers': ['Nike', 'Adidas', 'Puma', 'Vans'],
     },
     {
-      'questionText': 'Apa peliharaan favorit anda?',
-      'answers': ['Anjing', 'kucing', 'Burung', 'Ular'],
+      'questionText': 'Apa kendaraan kesukaan anda?',
+      'answers': ['Mobil', 'Motor', 'Pesawat', 'Sepeda'],
     },
     {
-      'questionText': 'Berapa jumlah peliharaan anda?',
-      'answers': ['1 ekor', '2 ekor', '3 ekor', '4 ekor'],
+      'questionText': 'Siapa pemain basket kesukaan anda?',
+      'answers': [
+        'Lebron James',
+        'Damian Lilliard',
+        'Kevin Durant',
+        'Kyrie Irving'
+      ],
     },
- ];
- var _questionIndex = 0;
- 
- void _answerQuestion() {
-   // var aBool = true;
-   // aBool = false;
- 
-   setState(() {
-     _questionIndex = _questionIndex + 1;
-   });
-   print(_questionIndex);
-   if (_questionIndex < _questions.length) {
+  ];
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    // var aBool = true;
+    // aBool = false;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
       print('Kita masih punya pertanyaan!');
-   } else {
-     print('tidak ada pertanyaan lagi!');
-   }
- }
- 
- @override
- Widget build(BuildContext context) {
-   // var dummy = const ['Hello'];
-   // dummy.add('Max');
-   // print(dummy);
-   // dummy = [];
-   // questions = []; // does not work if questions is a const
- 
-   return MaterialApp(
-     home: Scaffold(
-       appBar: AppBar(
-         title: Text('Aplikasi quiz'),
-       ),
-       body: _questionIndex < _questions.length
-           ? Quiz(
-               answerQuestion: _answerQuestion,
-               questionIndex: _questionIndex,
-               questions: _questions,
-             )
-           : Result(),
-     ),
-   );
- }
+    } else {
+      print('tidak ada pertanyaan lagi!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // var dummy = const ['Hello'];
+    // dummy.add('Max');
+    // print(dummy);
+    // dummy = [];
+    // questions = []; // does not work if questions is a const
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Aplikasi quiz'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
+      ),
+    );
+  }
 }
